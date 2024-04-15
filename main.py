d4b753358241b27def7138ae87084fa784036e9c
Sara Hau <s.hau@mci4me.at>
2024-04-15 15:49:37 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,28 @@
+import streamlit as st
+from read_data import get_person_data, get_person_names
+from PIL import Image
+
+person_data = get_person_data()
+person_names_list = get_person_names(person_data)
+
+# Session State wird leer angelegt, solange er noch nicht existiert
+if 'current_user' not in st.session_state:
+    st.session_state.current_user = 'None'
+
+# Eine Überschrift der ersten Ebene
+st.write("# EKG APP")
+
+# Eine Überschrift der zweiten Ebene
+st.write("## Versuchsperson auswählen")
+
+# Eine Auswahlbox, das Ergebnis wird in current_user gespeichert
+st.session_state.current_user = st.selectbox(
+    'Versuchsperson',
+    options = person_names_list, key="sbVersuchsperson")
+
+# Laden eines Bilds
+image = Image.open("data/pictures/js.jpg")
+# Anzeigen eines Bilds mit Caption
+st.image(image, caption=st.session_state.current_user)
+
+st.write("Es wurde folgender Nutzer gewählt: " + st.session_state.current_user)
---
+++ read_data.py
@@ -0,0 +1,49 @@
+import json
+
+def get_person_data():
+# Opening JSON file
+    file = open("data/person_db.json")
+
+# Loading the JSON File in a dictionary
+    person_data = json.load(file)
+    return person_data
+
+def get_person_names(person_data):
+    """gets all names from the person_data and returns them as a list"""
+
+    #person_data = get_person_data()
+
+    #print(person_data)
+    list_with_person_names = []
+    for person in person_data:
+        list_with_person_names.append(person['lastname'] + ' , ' + person['firstname'])
+    #print(list_with_person_names)
+
+    return list_with_person_names
+
+def find_person_data_by_name(suchstring):
+   
+    person_data = get_person_data()
+    if suchstring == "None":
+        return {}
+
+    two_names = suchstring.split(" , ")
+    vorname = two_names[1]
+    nachname = two_names[0]
+
+    for eintrag in person_data:
+        if (eintrag["lastname"] == nachname and eintrag["firstname"] == vorname):
+            #print(eintrag)
+    
+            return eintrag
+    else:
+        return {}
+
+if __name__ == "__main__" :
+    person_data = get_person_data()
+    person_names_list = get_person_names(person_data)
+    #print(person_names_list)
+    suchstring ='Schmirander , Yunus'
+    print(find_person_data_by_name(suchstring))
+
+
