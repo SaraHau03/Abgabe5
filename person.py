757e1492babf812711c654566cef8a054e20534f
Sara Hau <s.hau@mci4me.at>
2024-05-27 11:44:34 +0200

    falsch

--- person.py
+++ person.py
@@ -15,7 +15,6 @@ class Person:
     def get_person_list(person_data):
         """A Function that takes the persons-dictionary and returns a list auf all person names"""
         list_of_names = []
-
         for eintrag in person_data:
             list_of_names.append(eintrag["lastname"] + ", " +  eintrag["firstname"])
         return list_of_names
@@ -50,6 +49,7 @@ class Person:
         self.picture_path = person_dict["picture_path"]
         self.id = person_dict["id"]
         
+        
 
     def calc_age(self):
         today = datetime.now()
@@ -61,10 +61,8 @@ class Person:
     def calc_max_heart_rate(self):
         max_hr_bpm =  223 - 0.9 * self.calc_age()
         return int(max_hr_bpm)
-
     
     def load_by_id(self):
-        
 
 if __name__ == "__main__":
     print("This is a module with some functions to read the person data")
@@ -74,6 +72,4 @@ if __name__ == "__main__":
     print(Person.find_person_data_by_name("Heyer, Yannic"))
     selected_person = Person(Person.find_person_data_by_name("Heyer, Yannic"))
     print(selected_person.calc_max_heart_rate())
-    
-
-    
+    print(selected_person.load_by_id())
