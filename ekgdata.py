36a7fb857e0eaeeb125360f382c8bd8bbb260b20
Sara Hau <s.hau@mci4me.at>
2024-05-27 12:18:40 +0200

    weitergekommen

--- ekgdata.py
+++ ekgdata.py
@@ -1,5 +1,6 @@
 import json
 import pandas as pd
+import person
 
 # %% Objekt-Welt
 
@@ -16,7 +17,24 @@ class EKGdata:
         self.data = ekg_dict["result_link"]
         self.df = pd.read_csv(self.data, sep='\t', header=None, names=['EKG in mV','Time in ms',])
 
+    @staticmethod
+    def find_ekg_data_by_id(self):
+        ekg_data = self.df
+        """A Function that takes an ID and returns the person as a dictionary"""
+        for eintrag in ekg_data:
+            if eintrag["id"] == ekg_data:
+                return eintrag
+        return {}
+    
+    @classmethod
+    def load_by_id(cls, ekg_dict):
+        ekgdict = cls.find_ekg_data_by_id(ekg_dict)
+        if ekgdict:
+            return cls(ekgdict)
+        else:
+            return None
 
+        
 
 if __name__ == "__main__":
     print("This is a module with some functions to read the EKG data")
@@ -26,3 +44,74 @@ if __name__ == "__main__":
     print(ekg_dict)
     ekg = EKGdata(ekg_dict)
     print(ekg.df.head())
+
+    try:
+        ekg_id_input = int(input("Bitte geben Sie die ID der Person ein: "))
+        ekg_by_id = EKGdata.load_by_id(ekg_id_input)
+        if ekg_by_id:
+            print(f"Person loaded by ID:", ekg_by_id)
+            #print(f"Max Herzfrequenz: {ekg_by_id.calc_max_heart_rate()} bpm")
+        else:
+            print("Keine Person mit der gegebenen ID gefunden.")
+    except ValueError:
+        print("Bitte geben Sie eine gültige numerische ID ein.")
+
+###################
+class EKGdata:
+    # Class variable to hold all EKG data
+    all_ekg_data = []
+
+    def __init__(self, ekg_dict):
+        self.id = ekg_dict["id"]
+        self.date = ekg_dict["date"]
+        self.data = ekg_dict["result_link"]
+        self.df = pd.read_csv(self.data, sep='\t', header=None, names=['EKG in mV', 'Time in ms'])
+
+    @staticmethod
+    def find_ekg_data_by_id(ekg_id):
+        """A Function that takes an ID and returns the EKG data as a dictionary"""
+        for ekg_dict in EKGdata.all_ekg_data:
+            if ekg_dict["id"] == ekg_id:
+                return ekg_dict
+        return None
+
+    @classmethod
+    def load_by_id(cls, ekg_id):
+        ekg_dict = cls.find_ekg_data_by_id(ekg_id)
+        if ekg_dict:
+            return cls(ekg_dict)
+        else:
+            return None
+
+    def display(self):
+        print(f"ID: {self.id}")
+        print(f"Date: {self.date}")
+        print(f"Data File: {self.data}")
+        print(f"EKG Data (first 5 rows):\n{self.df.head()}")
+
+if __name__ == "__main__":
+    print("This is a module with some functions to read the EKG data")
+    
+    # Load person data and populate all_ekg_data class variable
+    with open("data/person_db.json") as file:
+        person_data = json.load(file)
+    
+    for person in person_data:
+        EKGdata.all_ekg_data.extend(person["ekg_tests"])
+    
+    # Example usage
+    ekg_dict = person_data[0]["ekg_tests"][0]
+    print(ekg_dict)
+    ekg = EKGdata(ekg_dict)
+    print(ekg.df.head())
+
+    try:
+        ekg_id_input = int(input("Bitte geben Sie die ID der Person ein: "))
+        ekg_by_id = EKGdata.load_by_id(ekg_id_input)
+        if ekg_by_id:
+            print("EKG Data loaded by ID:")
+            ekg_by_id.display()
+        else:
+            print("Keine EKG-Daten mit der gegebenen ID gefunden.")
+    except ValueError:
+        print("Bitte geben Sie eine gültige numerische ID ein.")
--- test.py
+++ test.py
@@ -1,103 +1,61 @@
 import json
-from datetime import datetime
 import pandas as pd
 
-class Person:
-    
-    @staticmethod
-    def load_person_data():
-        """A Function that knows where te person Database is and returns a Dictionary with the Persons"""
-        file = open("data/person_db.json")
-        person_data = json.load(file)
-        return person_data
+class EKGdata:
+    # Class variable to hold all EKG data
+    all_ekg_data = []
+
+    def __init__(self, ekg_dict):
+        self.id = ekg_dict["id"]
+        self.date = ekg_dict["date"]
+        self.data = ekg_dict["result_link"]
+        self.df = pd.read_csv(self.data, sep='\t', header=None, names=['EKG in mV', 'Time in ms'])
 
     @staticmethod
-    def get_person_list(person_data):
-        """A Function that takes the persons-dictionary and returns a list auf all person names"""
-        list_of_names = []
-
-        for eintrag in person_data:
-            list_of_names.append(eintrag["lastname"] + ", " +  eintrag["firstname"])
-        return list_of_names
-    
-    @staticmethod
-    def find_person_data_by_name(suchstring):
-        """ Eine Funktion der Nachname, Vorname als ein String übergeben wird
-        und die die Person als Dictionary zurück gibt"""
-
-        person_data = Person.load_person_data()
-        #print(suchstring)
-        if suchstring == "None":
-            return {}
-
-        two_names = suchstring.split(", ")
-        vorname = two_names[1]
-        nachname = two_names[0]
-
-        for eintrag in person_data:
-            print(eintrag)
-            if (eintrag["lastname"] == nachname and eintrag["firstname"] == vorname):
-                print()
-
-                return eintrag
-        else:
-            return {}
-        
-    @staticmethod
-    def find_person_data_by_id(person_id):
-        """A Function that takes an ID and returns the person as a dictionary"""
-        person_data = Person.load_person_data()
-        for eintrag in person_data:
-            if eintrag["id"] == person_id:
-                return eintrag
-        return {}
-        
-    def __init__(self, person_dict) -> None:
-        self.date_of_birth = person_dict["date_of_birth"]
-        self.firstname = person_dict["firstname"]
-        self.lastname = person_dict["lastname"]
-        self.picture_path = person_dict["picture_path"]
-        self.id = person_dict["id"]
-        
-
-    def calc_age(self):
-        today = datetime.now()
-        age = today.year - self.date_of_birth
-        self.age = age
-        return age
-    
-
-    def calc_max_heart_rate(self):
-        max_hr_bpm =  223 - 0.9 * self.calc_age()
-        return int(max_hr_bpm)
+    def find_ekg_data_by_id(ekg_id):
+        """A Function that takes an ID and returns the EKG data as a dictionary"""
+        for ekg_dict in EKGdata.all_ekg_data:
+            if ekg_dict["id"] == ekg_id:
+                return ekg_dict
+        return None
 
     @classmethod
-    def load_by_id(cls, person_id):
-        person_dict = cls.find_person_data_by_id(person_id)
-        if person_dict:
-            return cls(person_dict)
+    def load_by_id(cls, ekg_id):
+        ekg_dict = cls.find_ekg_data_by_id(ekg_id)
+        if ekg_dict:
+            return cls(ekg_dict)
         else:
             return None
-        
-        
+
+    def display(self):
+        print(f"ID: {self.id}")
+        print(f"Date: {self.date}")
+        print(f"Data File: {self.data}")
+        print(f"EKG Data (first 5 rows):\n{self.df.head()}")
 
 if __name__ == "__main__":
-    print("This is a module with some functions to read the person data")
-    persons = Person.load_person_data()
-    person_names = Person.get_person_list(persons)
-    print(person_names)
-    print(Person.find_person_data_by_name("Heyer, Yannic"))
-    selected_person = Person(Person.find_person_data_by_name("Heyer, Yannic"))
-    print(selected_person.calc_max_heart_rate())
+    print("This is a module with some functions to read the EKG data")
     
+    # Load person data and populate all_ekg_data class variable
+    with open("data/person_db.json") as file:
+        person_data = json.load(file)
+    
+    for person in person_data:
+        EKGdata.all_ekg_data.extend(person["ekg_tests"])
+    
+    # Example usage
+    ekg_dict = person_data[0]["ekg_tests"][0]
+    print(ekg_dict)
+    ekg = EKGdata(ekg_dict)
+    print(ekg.df.head())
 
     try:
-        person_id_input = int(input("Bitte geben Sie die ID der Person ein: "))
-        person_by_id = Person.load_by_id(person_id_input)
-        if person_by_id:
-            print(f"Person loaded by ID: {person_by_id.firstname} {person_by_id.lastname}")
-            print(f"Max Herzfrequenz: {person_by_id.calc_max_heart_rate()} bpm")
+        ekg_id_input = int(input("Bitte geben Sie die ID der Person ein: "))
+        ekg_by_id = EKGdata.load_by_id(ekg_id_input)
+        if ekg_by_id:
+            print("EKG Data loaded by ID:")
+            ekg_by_id.display()
         else:
-            print("Keine Person mit der gegebenen ID gefunden.")
+            print("Keine EKG-Daten mit der gegebenen ID gefunden.")
     except ValueError:
         print("Bitte geben Sie eine gültige numerische ID ein.")
